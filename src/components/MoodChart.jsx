a650f5d862d661ebe4c8a3d2079ed943b14cde76
MargoTech <margobartashevich@gmail.com>
2025-05-17 17:00:26 +0200

    Working with MoodChart

--- src/components/MoodChart.jsx
+++ src/components/MoodChart.jsx
@@ -1,11 +1,36 @@
 import { useMood } from "../context/MoodContext";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
 
-function MoodChart() {
+const moodToNumber = (mood) => {
+  const map = {
+    Great: 5,
+    Good: 4,
+    Okay: 3,
+    "Not Great": 2,
+    Bad: 1,
+  };
+  return map[mood] ?? 0;
+};
+
+export default function MoodChart() {
   const { moods } = useMood();
-  const moodCounts = moods.reduce((acc, { mood }) => {
-    acc[mood] = (acc[mood] || 0) + 1;
-    return acc;
-  }, {});
+
+  const data = moods.map(({ mood, date }) => ({
+    date,
+    mood: moodToNumber(mood),
+  }));
+
+  // const moodCounts = moods.reduce((acc, { mood }) => {
+  //   acc[mood] = (acc[mood] || 0) + 1;
+  //   return acc;
+  // }, {});
 
   return (
     <div>
@@ -20,4 +45,3 @@ function MoodChart() {
     </div>
   );
 }
-export default MoodChart;
